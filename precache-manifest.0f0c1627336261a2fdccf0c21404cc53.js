784aadef3ad57a727b8f002cdfc88a12e5f81355
bootsrap <adhiltajudeen@gmail.com>
2022-08-30 01:36:22 +0530

    Updates

---
+++ precache-manifest.0f0c1627336261a2fdccf0c21404cc53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2fe45a22fa4cf6ddaa",
+    "url": "/Netfilx-app/static/css/main.e901f6c5.chunk.css"
+  },
+  {
+    "revision": "6c2fe45a22fa4cf6ddaa",
+    "url": "/Netfilx-app/static/js/main.6c2fe45a.chunk.js"
+  },
+  {
+    "revision": "6d9fcdf2f44794300c2a",
+    "url": "/Netfilx-app/static/js/1.6d9fcdf2.chunk.js"
+  },
+  {
+    "revision": "b2cd18169c86379e5955",
+    "url": "/Netfilx-app/static/js/runtime~main.b2cd1816.js"
+  },
+  {
+    "revision": "f9818792b7faacc9e6e18dd4ffe2a7a3",
+    "url": "/Netfilx-app/index.html"
+  }
+];
